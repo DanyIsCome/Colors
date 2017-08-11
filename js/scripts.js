3d805f4ca8a7c46467f802f3a7c87daf72d87bbd
Matteo Danieli <matteo.danieli98@gmail.com>
2017-08-11 17:04:05 +0200

    Aggiunto play on hover a tutti i colori

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,44 @@
 jQuery( document ).ready(function() {
 
     //var myVideo=$("#videoMain");
-    var myVideo = document.getElementById("whiteVideo"); 
+    var videoWhite = document.getElementById("whiteVideo"); 
+    var videoRed = document.getElementById("redVideo"); 
+    var videoBlack = document.getElementById("blackVideo"); 
+    var videoYellow = document.getElementById("yellowVideo"); 
 
-    function play() { 
+    function play(myVideo) { 
             myVideo.play(); 
     } 
     
-    function pause() { 
+    function pause(myVideo) { 
             myVideo.pause(); 
     } 
-
+//White
     jQuery( "#whiteCont" ).on('mouseenter',function() {
-        play();  
+        play(videoWhite);  
     })
     jQuery( "#whiteCont" ).on('mouseleave',function() {
-        pause();  
+        pause(videoWhite);  
     })
-    
+//Red
+    jQuery( "#redCont" ).on('mouseenter',function() {
+        play(videoRed);  
+    })
+    jQuery( "#redCont" ).on('mouseleave',function() {
+        pause(videoRed);  
+    })
+//Black
+    jQuery( "#blackCont" ).on('mouseenter',function() {
+        play(videoBlack);  
+    })
+    jQuery( "#blackCont" ).on('mouseleave',function() {
+        pause(videoBlack);  
+    })
+//Yellow
+    jQuery( "#yellowCont" ).on('mouseenter',function() {
+        play(videoYellow);  
+    })
+    jQuery( "#yellowCont" ).on('mouseleave',function() {
+        pause(videoYellow);  
+    })
 })
